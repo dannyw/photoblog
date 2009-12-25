b8362aedc6a8e9a10d4a87bde8e9fe5430cb0b45
Danny Wen <danny@iridesco.com>
2009-12-25 01:59:04 -0500

    downgrade rails for damn dreamhost

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+default_run_options[:pty] = true
+
+# be sure to change these
+set :user, 'armitage'
+set :domain, 'photoblog.plasticity.com'
+set :application, 'photoblog'
+
+# the rest should be good
+set :repository,  "git@github.com:dannyw/photoblog.git"
+set :deploy_to, "/home/#{user}/#{domain}" 
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
+
+server domain, :app, :web
+role :db, domain, :primary => true
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+end
